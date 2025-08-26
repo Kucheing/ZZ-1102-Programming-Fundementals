5ea28126156b270b832d12b2302b5654e319682a
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2025-08-26 08:18:41 +0800

    done with task 7 of lab 2-3

---
+++ Lab2-3 - Loops & Methods (Void & Value)/Task7/getWLRectangle.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class getWLRectangle {
+
+    public static void main(String[] args) {
+        double length = getLength();
+        double width = getWidth();
+        double area = getArea(length, width);
+        displayData(length, width, area);
+
+    }
+
+    private static double getLength() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle: ");
+        return input.nextDouble();
+    }
+
+    private static double getWidth() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle: ");
+        return input.nextDouble();
+    }
+
+    private static double getArea(double length, double width) {
+        double area = length * width;
+        System.out.printf("The area of the rectangle is: %.2f%n", area);
+        return area;
+    }
+
+    private static void displayData(double length, double width, double area) {
+        System.out.printf("The entered Length is: %.2f%nThe entered Width is: %.2f%nThe area is: %.2f%n", length, width, area);
+    }
+}
---
+++ Lab2-3 - Loops & Methods (Void & Value)/Task7/task.md
@@ -0,0 +1,8 @@
+Task07: Write a program that will ask the user to enter the width and length of a rectangle, and then display the rectangle’s area.
+The program calls the following methods, which have not been written:
+• getLength—This method should ask the user to enter the rectangle’s length, and then return that value as a double.
+• getWidth—This method should ask the user to enter the rectangle’s width, and then return that value as a double.
+• getArea—This method should accept the rectangle’s length and width as arguments, and return the rectangle’s area. The area is
+calculated by multiplying the length by the width.
+• displayData—This method should accept the rectangle’s length, width, and area as arguments, and display them in an appropriate
+message on the screen
